72073e2dd17645bbea1b1b24a25e552f00cc642a
haruipsak <zzzdahee@naver.com>
2024-07-26 17:24:31 +0900

    2024-07-26 noticeSearch부분

---
+++ src/component/page/Notice/NoticeSearch/NoticeSearch.tsx
@@ -0,0 +1,8 @@
+export const NoticeSarch = () => {
+  return (
+    <>
+      라우터에서 연결된 페이지에있는 노티스 페이지안에 있는 컴포넌트인
+      NoticeSearch가 띄워짐
+    </>
+  );
+};
---
+++ src/pages/Notice.tsx
@@ -0,0 +1,9 @@
+import { NoticeSarch } from "../component/page/Notice/NoticeSearch/NoticeSearch";
+
+export const Notice = () => {
+  return (
+    <>
+      <NoticeSarch />
+    </>
+  );
+};
